81782003ebd93a85d62e1d1635170b1d87b75891
Rabiul <rabiul35-487@diu.edu.bd>
2021-11-21 01:30:18 +0600

    Happy Coding

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+My_information = {
+    "name" : "MD Rabiul awal shuvo",
+    "Study":"BSc in Software Engineering",
+    "email":"rabiul35-487",
+    "gender":"Male",
+    "is_verified":True,
+    "Birthdate":"01-01-2001",
+    "age" : 18,
+    }
+print(My_information["Study"])
