5918707a5f1ca415aec1ce0f480a1740075aa695
Rabiul <rabiul35-487@diu.edu.bd>
2021-11-21 19:58:06 +0600

    Happy Coding

--- import_function.py
+++ import_function.py
@@ -1,7 +1,7 @@
 from max_number import find_max
 from max_number import find_min
-
 numbers = [10,3,6,2]
 max = find_max(numbers)
 min = find_min(numbers)
+print(numbers)
 print(f"Maximum number is {max} \nAnd Minimum Number is {min}")
